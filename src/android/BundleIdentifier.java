7f267163e45b03bd26473c4942519f7a80c4f238
Antonio García <antonio@lostium.com>
2013-10-21 12:55:24 +0200

    Added Android support to get BundleId (package name in Android)

---
+++ src/android/BundleIdentifier.java
@@ -0,0 +1,22 @@
+package com.wunwun.plugins.bundleidentifier.BundleIdentifier;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONException;
+
+public class BundleIdentifier extends CordovaPlugin {
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callback) {
+        try {
+            JSONObject r = new JSONObject();
+            r.put("bundleId", this.cordova.getActivity().getApplicationContext().getPackageName());
+            callback.success(r);
+            return true;
+        } catch (JSONException ex) {
+            return false;
+        }
+    }
+}
