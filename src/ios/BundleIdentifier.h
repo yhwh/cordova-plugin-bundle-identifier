a175c451965d1845cd75ce4ae4eb5c9de0e5caba
Nicolas Ferrero <ferrero.nicolas@gmail.com>
2016-06-06 15:43:18 +0100

    cleanup

--- src/ios/BundleIdentifier.h
+++ src/ios/BundleIdentifier.h
@@ -7,20 +7,16 @@
 
 #import <Foundation/Foundation.h>
 #import <Cordova/CDVPlugin.h>
-#import <Social/Social.h>
-#import <Accounts/Accounts.h>
 
 @interface BundleIdentifier : CDVPlugin {
 
   NSMutableDictionary* callbackIds;
-//    NSString * phoneNumber;
-//    NSString * messageText;
+
 
 }
 
 @property (nonatomic, retain) NSMutableDictionary* callbackIds;
-//@property (nonatomic, retain) NSString *phoneNumber;
-//@property (nonatomic, retain) NSString *messageText;
+
 
 - (void)get:(CDVInvokedUrlCommand*)command;
 
